eec14306152447797f89342eecfd8b2f082cd131
dimonn12 <dimonn12@hotmail.com>
2014-06-13 22:03:36 +0300

    tblUserAccount creation, postgre-sql jdb added

---
+++ mywalletapp/sql/create_tblUserAccount.sql
@@ -0,0 +1,15 @@
+﻿drop table tblUserAccount;
+drop sequence tblUserAccount_auto_id;
+
+CREATE SEQUENCE tblUserAccount_auto_id;
+
+create table tblUserAccount(
+	"id" bigint NOT NULL DEFAULT nextval('tblUserAccount_auto_id') primary key,
+	"login" varchar(64),
+	"password" varchar(32),
+	"firstname" varchar(32),
+	"lastname" varchar(32),
+	"salt" varchar(128),
+	"deleted" boolean,
+	"created" timestamptz,
+	"modified" timestamptz)
